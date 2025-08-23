d99f162f27e8c3952cf6d39ce1c015cc620399a3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-22 22:36:48 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+
 export const routes: Routes = [
-{ path: '', component: HomeComponent },
-{ path: 'movie/:id', component: MovieDetailComponent },
-{ path: '**', redirectTo: '' }
+  { path: '', component: HomeComponent },
+  { 
+    path: 'movie/:id', 
+    component: MovieDetailComponent,
+    data: { renderMode: 'server' }   // 👈 evita prerender y usa SSR dinámico
+  },
+  { path: '**', redirectTo: '' }
 ];
