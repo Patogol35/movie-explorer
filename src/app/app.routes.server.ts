eb2a0da0d245c90c152d0ca760713a9dd55d5b97
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-23 00:27:31 -0500

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',              // ✅ solo prerender la home
+    path: '',              // ✅ prerenderiza solo la home
     renderMode: RenderMode.Prerender
   },
   {
     path: 'movie/:id',     // 🚀 ruta dinámica => SSR bajo demanda
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',            // 🌐 todas las demás rutas => SSR dinámico
     renderMode: RenderMode.Server
   }
 ];
