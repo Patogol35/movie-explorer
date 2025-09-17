3fed8cf3776b166fd6e0bdde6604be101b19d1ab
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-17 07:31:47 -0500

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,35 +3,34 @@ import { CommonModule } from '@angular/common';
 import { MovieService } from '../../services/movie.service';
 import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
 import { SearchFilterComponent } from '../../components/search-filter/search-filter.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-selector: 'app-home',
-standalone: true,
-imports: [CommonModule, MatToolbarModule, MovieCardComponent, SearchFilterComponent],
-templateUrl: './home.component.html',
-styleUrls: ['./home.component.css']   // 👈 ahora apunta al archivo css externo
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MovieCardComponent, SearchFilterComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-filtered: any[] = [];
+  filtered: any[] = [];
 
-@ViewChild('sf') searchFilter?: SearchFilterComponent;
+  @ViewChild('sf') searchFilter?: SearchFilterComponent;
 
-constructor(private movieService: MovieService) {}
+  constructor(private movieService: MovieService) {}
 
-ngOnInit(): void {
-this.filtered = this.movieService.movies();
-const genres = this.movieService.getAllGenres();
-setTimeout(() => this.searchFilter?.setGenres(genres));
-}
+  ngOnInit(): void {
+    this.filtered = this.movieService.movies();
+    const genres = this.movieService.getAllGenres();
+    setTimeout(() => this.searchFilter?.setGenres(genres));
+  }
 
-onFilter({ query, genre }: { query: string; genre: string }) {
-const q = query.toLowerCase();
-const all = this.movieService.movies();
-this.filtered = all.filter(m => {
-const byTitle = m.title.toLowerCase().includes(q);
-const byGenre = !genre || m.genres.includes(genre);
-return byTitle && byGenre;
-});
-}
+  onFilter({ query, genre }: { query: string; genre: string }) {
+    const q = query.toLowerCase();
+    const all = this.movieService.movies();
+    this.filtered = all.filter(m => {
+      const byTitle = m.title.toLowerCase().includes(q);
+      const byGenre = !genre || m.genres.includes(genre);
+      return byTitle && byGenre;
+    });
+  }
 }
