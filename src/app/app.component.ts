3f8ef9a5b085bb27401a73b08cf81e91511d422c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-17 07:24:12 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  isDark = false;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    const html = document.querySelector('html');
+    if (this.isDark) {
+      html?.classList.add('dark-theme');
+    } else {
+      html?.classList.remove('dark-theme');
+    }
+  }
+        }
